e35dec146250576187de2f41f3a7752ca8d6ba2e
AREF BAHMANI <arefbahmani021@gmail.com>
2024-10-31 02:43:20 +0330

    Add project files.

---
+++ Commend Project/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Commend_Project
+{
+    public interface IUserRepository
+    {
+        public void Add(User user);
+        public List<User> GetAll();
+        public User Get(string username);
+        public void Update(string userName, string password, string status);
+        public void Delete(string userName);
+
+    }
+}
---
+++ Commend Project/User.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Commend_Project
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Status { get; set; } = "available";
+
+    }
+}
